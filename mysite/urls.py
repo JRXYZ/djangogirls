e7d73230fd43f0240a8ac4b4b27467c16730a11f
admin <jorge@gmail.com>
2016-04-09 18:34:30 -0500

    Added views to create/edit blog post inside the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url
+from django.conf import settings
 from django.contrib import admin
-
+from django.conf.urls.static import static
 urlpatterns = [
     #Examples:
     #url(r'^$', 'mysite.views.home', name='home'),
     url(r'', include('blog.urls')),
 	url(r'^admin/', include(admin.site.urls)),
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
